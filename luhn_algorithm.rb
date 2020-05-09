261853d1ff765b08e405f3ff29adb6b3ba286545
Kostadin Politov <kostadin.politov@gmail.com>
2020-05-09 20:47:27 +0300

    Add files via upload
    Когато го коригирам ще го кача пак.

--- luhn_algorithm.rb
+++ luhn_algorithm.rb
@@ -1,4 +1,5 @@
 
+
 account_number = ARGV.first
 digits = []
 max_size = account_number.length().to_i
@@ -8,45 +9,77 @@ class Card_number
     @array_elements = array_elements
     @entered_number = entered_number
     @array_size = array_size
+    @counter_array = 0
+    @counter_calculations = 0
+    @sum1 = 0
+    @sum2 = 0
+    @sum3 = 0
+  end
+
+  def number_check
+    get_array
+    reverse_array
+    calculations
+    result1
+    check_result
   end
 
   def get_array
-  @array_elements[@array_size] = 1
-  counter = 0
+    @array_elements[@array_size - 1] = 1
     @array_elements.each do |digit|
-    @array_elements[counter] = @entered_number[counter]
-    counter += 1
+    @array_elements[@counter_array] = @entered_number[@counter_array]
+    @counter_array += 1 
     end
-  @array_elements = @array_elements.reverse
+  end
+
+  def reverse_array
+    @array_elements = @array_elements.reverse
   end
 
   def calculations
-    string_counter = 0
-	array_counter = 0
-	sum3 = 0
-	@array_elements.slice!(0)
-	  @array_elements.each do |digit|
-   	  sum1 = 0
-   	  sum2 = 0
-   	  if string_counter%2 == 1 
-        sum1 = 2*@array_elements[array_counter].to_i
- 	 	if sum1 >= 10
- 	  	 first_digit = sum1.to_s[0]
- 	  	 secound_digit = sum1.to_s[1] 
- 	  	 sum2 = first_digit.to_i + secound_digit.to_i
- 		else 
- 		 sum2 = sum1.to_i
- 	 	end
-   	  else string_counter%2 == 0
-        sum2 = @array_elements[array_counter]
-   	  end
-   	  sum3 += sum2.to_i
-   	  string_counter += 1
-   	  array_counter += 1
-      end
-	sum = sum3.to_i - @array_elements[0].to_i 
-	return @result = (sum*9)%10
+    @array_elements.each do |digit|
+    calculations1
+    @sum3 += @sum2.to_i
+    @counter_calculations += 1
+    end
   end
+
+  def calculations1
+    if @counter_calculations%2 == 1
+      calculations2
+    else @counter_calculations%2 == 0
+      calculations3
+    end
+  end
+
+  def calculations2
+    @sum1 = 2*@array_elements[@counter_calculations].to_i
+    if @sum1 >= 1
+      calculations21
+    else 
+      calculations22
+    end
+  end
+
+  def calculations21
+    first_digit = @sum1.to_s[0]
+    secound_digit = @sum1.to_s[1]
+    @sum2 = first_digit.to_i + secound_digit.to_i
+  end
+
+  def calculations22
+    @sum2 = @sum1
+  end
+
+  def calculations3
+    @sum2 = @array_elements[@counter_calculations]
+  end
+      
+
+  def result1
+    sum = @sum3.to_i - @array_elements[0].to_i 
+    return @result = (sum*9)%10
+  end 
 
   def check_result
     (12..18).cover?(@array_size) && @array_elements[0].to_i == @result 
@@ -55,7 +88,5 @@ end
 
 generate_number = Card_number.new
 generate_number.set_number(digits, account_number, max_size)
-digits = generate_number.get_array
-result = generate_number.calculations
-check = generate_number.check_result
-puts check
+result = generate_number.number_check
+puts result
